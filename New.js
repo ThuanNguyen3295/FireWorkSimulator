3e3e6cfe06392f8dbc2b2dd96d12c4117615a685
thuannguyen <ttn173@ucsd.edu>
2017-11-10 22:40:29 -0800

    Finishing!!!

--- New.js
+++ New.js
@@ -1,25 +1,24 @@
-var fireworks = [];
+var fireworks = []; // an array of Firework
 var gravity;
 
 function setup(){
-  createCanvas(500,300);
+  createCanvas(1000,600); // create the canvas
   gravity = createVector(0, 0.2);
   stroke(255);
   strokeWeight(4);
   background(0); // trailing for the particles
 }
 function draw(){
-  colorMode(RGB);
+  colorMode(RGB); // set up colorMode
   background(0, 0, 0, 25); // transition background
   if(random(1) < 0.04) {
   fireworks.push(new Firework());
 }
-  for(var i = fireworks.length -1; i >=0; i--){
+  for(var i = fireworks.length -1; i >=0; i--){ // loop through to update 
     fireworks[i].update();
     fireworks[i].show();
-    if(fireworks[i].done()){
+    if(fireworks[i].done()){ // if expoded, then remove it from the array
         fireworks.splice(i,1);
     }
   }
-  console.log(fireworks.length);
 }
--- Particle.js
+++ Particle.js
@@ -1,24 +1,25 @@
 function Particle(x,y, hu, firework){
-   this.pos = createVector(x,y);
+   this.pos = createVector(x,y); // starting position
    this.firework = firework;
-   this.lifespan = 255;
-   this.hu = hu;
-   if (this.firework) {
+   this.lifespan = 255; //lifespan per frame
+   this.hu = hu; //colorMode
+    if (this.firework) {
       this.vel = createVector(0, random(-12, -8));
     }
     else {
-        this.vel = p5.Vector.random2D();
+        this.vel = p5.Vector.random2D(); // simulate the explosion
         this.vel.mult(random(2.5,6));
     }
       this.acc = createVector(0,0);
-      this.applyForce = function(force){
+
+    this.applyForce = function(force){
       this.acc.add(force);
     }
     this.update = function (){
        if (!this.firework){
          this.vel.mult(0.9);
          var a = random(2, 12);
-         this.lifespan -= a;
+         this.lifespan -= a; // reduce the lifespan per frame
        }
 
         this.vel.add(this.acc);
